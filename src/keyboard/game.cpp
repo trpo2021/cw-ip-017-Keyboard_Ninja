bbecb497612e70325f4b954ace5a577af8f38a40
Evilrake <kolyannevolyan@gmail.com>
2021-09-19 17:30:05 +0700

    smart current symbol lightning

--- src/keyboard/game.cpp
+++ src/keyboard/game.cpp
@@ -4,9 +4,14 @@
 #include <locale>
 #include <string>
 #include <vector>
-
 using namespace std;
 using namespace sf;
+
+int getSymbolWidth(Text text)
+{
+    cout << text.findCharacterPos(1).x << endl;
+    return text.findCharacterPos(2).x - text.findCharacterPos(1).x;
+}
 
 void startGame(sf::RenderWindow& window)
 {
@@ -14,30 +19,25 @@ void startGame(sf::RenderWindow& window)
     if (!keyboardTexture.loadFromFile("source/keyboard.png")) {
         cout << "Error, source/keyboard.png not found" << endl;
     }
-
     RectangleShape keyboard(Vector2f(1200, 400));
     keyboard.setPosition(Vector2f(360, 680));
     keyboard.setTexture(&keyboardTexture);
-
     Font font;
     if (!font.loadFromFile("source/OpenSans-Regular.ttf")) {
         cout << "Error, source/OpenSans-Regular.ttf not found" << endl;
     }
-
     Text symbols;
     symbols.setFont(font);
     symbols.setCharacterSize(40);
     symbols.setFillColor(Color::Black);
-
     string levelStr = "good luck and have a nice day!";
     symbols.setString(levelStr);
     symbols.setPosition(950, 295);
-
     RectangleShape display(Vector2f(1920, 50));
     display.setPosition(Vector2f(0, 300));
 
-    RectangleShape currentSymbol(Vector2f(30, 50));
-    currentSymbol.setPosition(Vector2f(947, 300));
+    RectangleShape currentSymbol(Vector2f(23, 50));
+    currentSymbol.setPosition(Vector2f(949, 300));
     currentSymbol.setFillColor(Color(250, 150, 100, 160));
 
     while (window.isOpen()) {
@@ -50,8 +50,12 @@ void startGame(sf::RenderWindow& window)
                 if (event.text.unicode < 128) {
                     if (static_cast<char>(event.text.unicode) == levelStr[0]) {
                         levelStr.erase(levelStr.begin());
+                        currentSymbol.setSize(
+                                Vector2f(getSymbolWidth(symbols), 50));
+                        currentSymbol.setFillColor(Color(250, 150, 100, 160));
+
                     } else {
-                        cout << "FAIL!!" << endl;
+                        currentSymbol.setFillColor(Color(240, 50, 50, 160));
                     }
                 }
             }
@@ -59,10 +63,8 @@ void startGame(sf::RenderWindow& window)
         window.draw(keyboard);
         window.draw(display);
         window.draw(currentSymbol);
-
         symbols.setString(levelStr);
         window.draw(symbols);
-
         window.display();
         sleep(milliseconds(1000 / 60));
         window.clear();
